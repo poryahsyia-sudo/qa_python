1196dfba1001f2a401cdc6cd596d9a068922430d
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-09-23 20:10:32 +0500

    добавила негативный тест на get_books_with_specific_genre

--- test_book.py
+++ test_book.py
@@ -95,3 +95,10 @@ class TestBooksCollector:
         collector.add_new_book('Тайны Коко')
         collector.set_book_genre('Тайны Коко', 'Фантастика')
         assert collector.get_books_with_specific_genre('Фантастика') == ['Тайны Коко']
+
+# Негативный тест
+    def test_get_books_with_specific_genre_negative(self):
+        collector = BooksCollector()
+        collector.add_new_book('Морозко')
+        collector.set_book_genre('Морозко', 'Сказка')
+        assert collector.get_books_with_specific_genre('Мультфильмы') == []
